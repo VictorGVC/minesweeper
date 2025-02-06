785afa5fedc9df36e168f324a3a55fc3c996e6d1
Victor Costa <victorstudy17@hotmail.com>
2025-02-06 20:06:33 -0300

    feat: add Cell base structure

---
+++ src/main/java/minesweeper/domain/model/Cell.java
@@ -0,0 +1,44 @@
+package minesweeper.domain.model;
+
+public class Cell {
+    private boolean mine;
+    private boolean flagged;
+    private boolean revealed;
+    private int adjacentMines;
+
+    public boolean isMine() {
+        return mine;
+    }
+
+    public void setMine(boolean mine) {
+        this.mine = mine;
+    }
+
+    public boolean isFlagged() {
+        return flagged;
+    }
+
+    public void setFlagged(boolean flagged) {
+        this.flagged = flagged;
+    }
+
+    public boolean isRevealed() {
+        return revealed;
+    }
+
+    public void setRevealed(boolean revealed) {
+        this.revealed = revealed;
+    }
+
+    public int getAdjacentMines() {
+        return adjacentMines;
+    }
+
+    public void setAdjacentMines(int adjacentMines) {
+        this.adjacentMines = adjacentMines;
+    }
+
+    public void toggleFlag() {
+
+    }
+}
