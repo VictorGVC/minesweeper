754abd59350d3133ba2a6b1dc2c3d19ed2c4883c
Victor Costa <victorstudy17@hotmail.com>
2025-02-06 20:07:21 -0300

    test: add tests for cell flag behavior

---
+++ src/test/java/minesweeper/domain/model/CellTest.java
@@ -0,0 +1,51 @@
+package minesweeper.domain.model;
+
+import org.instancio.Instancio;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.instancio.Select.field;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CellTest {
+
+    @Test
+    void toggleFlag_WhenFlaggedAndNotRevealed_CellIsNotFlagged() {
+
+        var cell = Instancio.of(Cell.class)
+                .set(field(Cell::isFlagged), true)
+                .set(field(Cell::isRevealed), false)
+                .create();
+
+        cell.toggleFlag();
+
+        assertFalse(cell.isFlagged());
+    }
+
+    @Test
+    void toggleFlag_WhenNotFlaggedNotRevealed_CellIsFlagged() {
+        var cell = Instancio.of(Cell.class)
+                .set(field(Cell::isFlagged), false)
+                .set(field(Cell::isRevealed), false)
+                .create();
+
+        cell.toggleFlag();
+
+        assertTrue(cell.isFlagged());
+    }
+
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    void toggleFlag_WhenRevealed_FlagKeepValue(boolean flag) {
+        var cell = Instancio.of(Cell.class)
+                .set(field(Cell::isFlagged), flag)
+                .set(field(Cell::isRevealed), true)
+                .create();
+
+        cell.toggleFlag();
+
+        assertTrue(cell.isFlagged());
+    }
+
+}
